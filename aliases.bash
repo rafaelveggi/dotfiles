3701695c1dd17b31cf29f33e679be7bb6515e561
Rafael Veggi <rafaelveggi@.gmail.com>
2024-03-05 09:45:27 +0000

    i3status

--- aliases.bash
+++ aliases.bash
@@ -2,9 +2,10 @@
 
 alias x="startx"
 alias grep="grep --color=auto "
-alias ls="lsd -lh"
-alias l="lsd -lhA "
-alias lt="lsd --tree "
+alias ls="ls --color=auto "
+alias l="ls -lh"
+alias la="ls -lhA "
+alias lt="tree -L 1 "
 alias cat="bat"
 alias vi="nvim"
 alias rc="source ~/.bashrc"
@@ -19,6 +20,7 @@ alias updatedb="sudo updatedb"
 alias dmesg="sudo dmesg"
 alias reboot="sudo reboot"
 alias shutdown="sudo shutdown -h now"
+alias zzz="sudo zzz"
 function __sudo() {
     if [[ $1 == 'vi' ]]; then
         # replaces vi for nvim for convenience
@@ -39,22 +41,22 @@ alias C="__git_commit "
 function __git_commit() { git commit -m "$*"; }
 
 
-function __man() {
-    if command man "$1" &>/dev/null; then
-        command man "$1"
-    else
-    	local domain="dyn.manpages.debian.org"
-        local url="https://${domain}/bookworm/debian-goodies/${1}.1.en.gz"
-        local http_code
-	echo "Fetching manual from ${domain}"
-        if ! http_code=$(curl -s -o /dev/null -w "%{http_code}" "$url"); then
-            echo "Error fetching page"
-        elif [ "$http_code" -eq 404 ]; then
-            command man "$1"
-        else
-            curl -s "$url" | command man -l
-        fi
-    fi
-}
-alias man="__man"
+#function __man() {
+#    if command man "$1" &>/dev/null; then
+#        command man "$1"
+#    else
+#    	local domain="dyn.manpages.debian.org"
+#        local url="https://${domain}/bookworm/debian-goodies/${1}.1.en.gz"
+#        local http_code
+#	echo "Fetching manual from ${domain}"
+#        if ! http_code=$(curl -s -o /dev/null -w "%{http_code}" "$url"); then
+#            echo "Error fetching page"
+#        elif [ "$http_code" -eq 404 ]; then
+#            command man "$1"
+#        else
+#            curl -s "$url" | command man -l
+#        fi
+#    fi
+#}
+#alias man="__man"
 
